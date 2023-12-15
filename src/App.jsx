18ff8e2e05617376e8c364f2203f3ff596a9a04c
ParvinMusayev <parvinmusayev1@gmail.com>
2023-12-15 18:05:02 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import Search from "./pages/Search/Search";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Products />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/search" element={<Search />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
