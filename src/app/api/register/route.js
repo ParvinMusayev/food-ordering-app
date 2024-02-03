feb1e7438eb69fd83b546c3c0bb18f5e106997d9
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-03 17:39:24 +0400

    feat: Add bcryptjs

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,10 @@
+import { User } from "@/models/User";
+import mongoose from "mongoose";
+
 export async function POST(req) {
-    return Response.json("true")
+  const body = await req.json();
+  mongoose.connect(process.env.MONGO_URL);
+  const createdUser = await User.create(body);
+
+  return Response.json(createdUser);
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,27 @@
-const { Schema } = require("mongoose");
+const { Schema, model, models } = require("mongoose");
 
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: {
-    type: String,
-    required: true,
-    validate: (pass) => {
-      if (!pass?.length || pass.lenght < 5) {
-        new Error("password must be at least 5 characters");
-      }
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+      validate: (pass) => {
+        if (!pass?.length || pass.lenght < 5) {
+          new Error("password must be at least 5 characters");
+        }
+      },
     },
   },
+  { timestamps: true }
+);
+
+UserSchema.post("validate", function (user) {
+  const notHashedPassword = user.password;
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(notHashedPassword, salt);
 });
 
 export const User = models?.User || model("User", UserSchema);
