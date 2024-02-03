e8bbe54fb1ce05d6f3340fe955ec262a50317fe2
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-03 19:00:00 +0400

    feat: add AuthLinks function

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,46 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 
+function AuthLinks({ status, userName }) {
+  if (status === "authenticated") {
+    return (
+      <>
+        <Link href={"/profile"} className="whitespace-nowrap">
+          Hello, {userName}
+        </Link>
+        <button
+          onClick={() => signOut()}
+          className="bg-primary rounded-full text-white px-8 py-2"
+        >
+          Logout
+        </button>
+      </>
+    );
+  }
+  if (status === "unauthenticated") {
+    return (
+      <>
+        <Link href={"/login"}>Login</Link>
+        <Link
+          href={"/register"}
+          className="bg-primary rounded-full text-white px-8 py-2"
+        >
+          Register
+        </Link>
+      </>
+    );
+  }
+}
+
 export default function Header() {
+  const session = useSession();
+  const status = session?.status;
+  const userData = session.data?.user;
+
+  let userName = userData?.name || userData?.email;
+
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-8 font-semibold text-gray-500">
@@ -13,13 +53,7 @@ export default function Header() {
         <Link href={""}>Contact</Link>
       </nav>
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-        <Link href={"/login"}>Login</Link>
-        <Link
-          href={"/register"}
-          className="bg-primary rounded-full text-white px-8 py-2 "
-        >
-          Register
-        </Link>
+        <AuthLinks status={status} userName={userName} />
       </nav>
     </header>
   );
