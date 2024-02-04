8a781c4c95fd408249ac2624ff4f9980e45d6318
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-04 21:59:11 +0400

    chore: uptade register route

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -7,12 +7,13 @@ export async function PUT(req) {
   mongoose.connect(process.env.MONGO_URL);
   const data = await req.json();
   const session = await getServerSession(authOptions);
-  console.log({ session, data });
 
   const email = session.user.email;
+
+  
   if ("name" in data) {
     //uptade user name
-    await User.uptadeOne({ email }, { name: data.name });
+    await User.updateOne({ email }, { name: data.name });
   }
 
   return Response.json(true);
