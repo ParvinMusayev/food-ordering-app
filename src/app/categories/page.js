454690cb062b4dfee990094773f67ff394259dbd
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-06 17:33:57 +0400

    chore:  uptade categories page

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -3,32 +3,49 @@
 import UserTabs from "@/components/layout/UserTabs";
 import { useEffect, useState } from "react";
 
+import { useProfile } from "@/components/UseProfile";
+
 export default function CategoriesPage() {
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [adminInfoLoading, setAdminInfoLoading] = useState(true)
+  const [newCategoryName, setNewCategoryName] = useState("");
 
-  useEffect(() => {
-    setAdminInfoLoading(true)
-    fetch("/api/profile").then((response) => {
-      response.json().then((data) => {
-        setIsAdmin(data.admin);
-        setAdminInfoLoading(false)
-      });
+  const { loading: profileLoading, data: profileData } = useProfile();
+
+  function handleNewCategorySubmit() {
+    ev.preventDefault();
+    fetch("/api/categories", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: newCategoryName }),
     });
-  }, []);
-
-  if(adminInfoLoading){
-    return 'Loading user info...'
   }
 
-  if(!isAdmin){
-    return "Not an admin"
+  if (profileLoading) {
+    return "Loading user info...";
+  }
 
+  if (!profileData.admin) {
+    return "Not an admin";
   }
   return (
     <section className="mt-8 max-w-2xl mx-auto">
       <UserTabs isAdmin={true} />
-      categories
+      <form className="mt-8" onSubmit={handleNewCategorySubmit}>
+        <div className="flex gap-2 items-end">
+          <div className="grow">
+            <label>New category name</label>
+            <input
+              value={newCategoryName}
+              onChange={(ev) => setNewCategoryName(ev.target.value)}
+              type="text"
+            />
+          </div>
+          <div className="pb-2 flex gap-2">
+            <button className="border border-primary" type="submit">
+              Create
+            </button>
+          </div>
+        </div>
+      </form>
     </section>
   );
 }
