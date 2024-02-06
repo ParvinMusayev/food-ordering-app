2d5d975b3ca4042077b81da83dd74f9922cc6b4a
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-06 17:04:25 +0400

    feat: Add UseProfile

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -2,16 +2,20 @@ import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { User } from "@/models/User";
+import { UserInfo } from "@/models/UserInfo";
 
 export async function PUT(req) {
   mongoose.connect(process.env.MONGO_URL);
   const data = await req.json();
+  const { name, ...otherUserInfo } = data;
   const session = await getServerSession(authOptions);
 
   const email = session.user.email;
 
   //uptade user name
-  await User.updateOne({ email }, data);
+  await User.updateOne({ email }, { name });
+
+  await UserInfo.findOneAndUpdate({ email }, otherUserInfo, { upsert: true });
 
   return Response.json(true);
 }
@@ -23,9 +27,15 @@ export async function GET(req) {
 
   const email = session?.user?.email;
 
-  if(!email){
-    return Response.json({})
+  if (!email) {
+    return Response.json({});
   }
 
-  return Response.json(await User.findOne({ email }));
+  const user = await User.findOne({ email }).lean();
+  const userInfo = await UserInfo.findOne({ email }).lean();
+
+  return Response.json({
+    ...user,
+    ...userInfo,
+  });
 }
--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -5,14 +5,21 @@ import { useEffect, useState } from "react";
 
 export default function CategoriesPage() {
   const [isAdmin, setIsAdmin] = useState(false);
+  const [adminInfoLoading, setAdminInfoLoading] = useState(true)
 
   useEffect(() => {
+    setAdminInfoLoading(true)
     fetch("/api/profile").then((response) => {
       response.json().then((data) => {
         setIsAdmin(data.admin);
+        setAdminInfoLoading(false)
       });
     });
   }, []);
+
+  if(adminInfoLoading){
+    return 'Loading user info...'
+  }
 
   if(!isAdmin){
     return "Not an admin"
