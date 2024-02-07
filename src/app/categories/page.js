d35bedfa7ab137684e71c63e548c933ebf7737bd
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-07 11:06:13 +0400

    chore: update categories page

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -6,17 +6,22 @@ import { useProfile } from "@/components/UseProfile";
 import toast from "react-hot-toast";
 
 export default function CategoriesPage() {
-  const [newCategoryName, setNewCategoryName] = useState("");
+  const [categoryName, setCategoryName] = useState("");
   const [categories, setCategories] = useState([]);
+  const [editedCategory, setEditedCategory] = useState(null);
   const { loading: profileLoading, data: profileData } = useProfile();
 
   useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  function fetchCategories() {
     fetch("/api/categories").then((res) => {
       res.json().then((categories) => {
         setCategories(categories);
       });
     });
-  }, []);
+  }
 
   async function handleNewCategorySubmit(ev) {
     ev.preventDefault();
@@ -25,8 +30,11 @@ export default function CategoriesPage() {
       const response = await fetch("/api/categories", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name: newCategoryName }),
+        body: JSON.stringify({ name: categoryName }),
       });
+
+      setCategoryName("");
+      fetchCategories();
 
       if (response.ok) resolve();
       else reject();
@@ -51,16 +59,21 @@ export default function CategoriesPage() {
       <form className="mt-8" onSubmit={handleNewCategorySubmit}>
         <div className="flex gap-2 items-end">
           <div className="grow">
-            <label>New category name</label>
+            {editedCategory ? "Update category" : "New category name"}
+            {editedCategory && (
+              <>
+                : <b>{editedCategory.name}</b>
+              </>
+            )}
             <input
-              value={newCategoryName}
-              onChange={(ev) => setNewCategoryName(ev.target.value)}
+              value={categoryName}
+              onChange={(ev) => setCategoryName(ev.target.value)}
               type="text"
             />
           </div>
           <div className="pb-2 flex gap-2">
             <button className="border border-primary" type="submit">
-              Create
+              {editedCategory ? "Update" : "Create"}
             </button>
           </div>
         </div>
@@ -70,11 +83,16 @@ export default function CategoriesPage() {
         {categories?.length > 0 &&
           categories.map((c) => (
             <div
+              onClick={() => {
+                setEditedCategory(c);
+                setCategoryName(c.name);
+              }}
+              
               className="bg-gray-100 rounded-xl p-2 px-4 flex gap-1 mb-1 items-center"
               key={c._id}
             >
               <div className="grow">{c.name}</div>
-              
+
               <div className="flex gap-1">
                 <button type="button">Edit</button>
               </div>
