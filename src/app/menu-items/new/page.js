8391659a37f193dce8130c58b4f69ed653e67da3
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-08 17:03:48 +0400

    chore: update menu item page

--- src/app/menu-items/new/page.js
+++ src/app/menu-items/new/page.js
@@ -12,6 +12,7 @@ import UserTabs from "@/components/layout/UserTabs";
 //icons
 import Left from "@/components/icons/Left";
 import Right from "@/components/icons/Right";
+import EditableImage from "@/components/layout/EditableImage";
 
 
 export default function NewMenuItemPage() {
@@ -27,7 +28,38 @@ export default function NewMenuItemPage() {
 
       return (
         <section className="mt-8">
-              <UserTabs isAdmin={true} />
-        </section>
+      <UserTabs isAdmin={true} />
+      <form onSubmit={handleFormSubmit} className="mt-8 max-w-2xl mx-auto ">
+        <div
+          style={{ gridTemplateColumns: ".3fr .7fr" }}
+          className="gird items-start gap-4"
+        >
+          <div>
+            <EditableImage link={image} setLink={setImage} />
+          </div>
+          <div className="grow">
+            <label>Item name</label>
+            <input
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+              type="text"
+            />
+            <label>Description</label>
+            <input
+              value={description}
+              onChange={(ev) => setDescription(ev.target.value)}
+              type="text"
+            />
+            <label>Base price</label>
+            <input
+              value={basePrice}
+              onChange={(ev) => setBasePrice(ev.target.value)}
+              type="text"
+            />
+            <button type="submit">Save</button>
+          </div>
+        </div>
+      </form>
+    </section>
       )
 }
--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -44,39 +44,6 @@ export default function MenuItemsPage() {
     return "Not an admin.";
   }
   return (
-    <section className="mt-8">
-      <UserTabs isAdmin={true} />
-      <form onSubmit={handleFormSubmit} className="mt-8 max-w-2xl mx-auto ">
-        <div
-          style={{ gridTemplateColumns: ".3fr .7fr" }}
-          className="gird items-start gap-4"
-        >
-          <div>
-            <EditableImage link={image} setLink={setImage} />
-          </div>
-          <div className="grow">
-            <label>Item name</label>
-            <input
-              value={name}
-              onChange={(ev) => setName(ev.target.value)}
-              type="text"
-            />
-            <label>Description</label>
-            <input
-              value={description}
-              onChange={(ev) => setDescription(ev.target.value)}
-              type="text"
-            />
-            <label>Base price</label>
-            <input
-              value={basePrice}
-              onChange={(ev) => setBasePrice(ev.target.value)}
-              type="text"
-            />
-            <button type="submit">Save</button>
-          </div>
-        </div>
-      </form>
-    </section>
+    <div>Menu items</div>
   );
 }
