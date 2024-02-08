1f7ca0a9bbfca55c8486160282c92400c1cf15f6
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-09 00:44:10 +0400

    fix: update edit page

--- src/app/menu-items/edit/[id]/page.js
+++ src/app/menu-items/edit/[id]/page.js
@@ -2,65 +2,107 @@
 
 import { useProfile } from "@/components/UseProfile";
 import Left from "@/components/icons/Left";
-import Right from "@/components/icons/Right";
+
+import EditableImage from "@/components/layout/EditableImage";
 import UserTabs from "@/components/layout/UserTabs";
-import Image from "next/image";
+
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function EditMenuItemPage() {
-    const { loading, data } = useProfile();
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [basePrice, setBasePrice] = useState("");
+  const [redirectToItems, setRedirectToItems] = useState(false);
+  const { loading, data } = useProfile();
 
-    const [menuItems, setMenuItems] = useState([]);
-  
-    useEffect(() => {
-      fetch("/api/menu-items").then((res) => {
-        res.json().then((menuItems) => {
-          setMenuItems(menuItems);
-        });
+  useEffect(() => {
+    fetch("/api/menu-items").then((res) => {
+      res.json().then((items) => {
+        const item = items.find((i) => i._id === id);
+        setMenuItem(item);
       });
-    }, []);
-  
-    if (loading) {
-      return "Loading user info...";
-    }
-  
-    if (!data.admin) {
-      return "Not an admin.";
-    }
-    return (
-      <section className="mt-8 max-w-2xl mx-auto">
-        <UserTabs isAdmin={true} />
-        <div className="mt-8">
-          <Link className="button flex" href={"/menu-items/new"}>
-            <span>Crete new menu item</span>
-            <Right />
-          </Link>
-        </div>
-        <div>
-          <h2 className="text-sm text-gray-500 mt-8">Edit menu item:</h2>
-          <div className="grid grid-cols-3 gap-2">
-            {menuItems?.length > 0 &&
-              menuItems.map((item) => (
-                <Link
-                  key={item._id}
-                  href={"/menu-items/edit/" + item._id}
-                  className="bg-gray-200 rounded-lg p-4"
-                >
-                  <div className="relative">
-                    <Image
-                      className="rounded-md"
-                      src={'/pizza.png'}
-                      alt={""}
-                      width={200}
-                      height={200}
-                    />
-                  </div>
-                  <div className="text-center">{item.name}</div>
-                </Link>
-              ))}
+    });
+  }, []);
+
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    const data = { image, name, description, basePrice };
+
+    const savingPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/menu-items", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) resolve();
+      else reject();
+    });
+
+    await toast.promise(savingPromise, {
+      loading: "Saving...",
+      success: "Profile saved!",
+      error: "Error",
+    });
+
+    setRedirectToItems(true);
+  }
+
+  if (redirectToItems) {
+    return redirect("/menu-items");
+  }
+
+  if (loading) {
+    return "Loading user info...";
+  }
+
+  if (!data.admin) {
+    return "Not an admin.";
+  }
+
+  return (
+    <section className="mt-8">
+      <UserTabs isAdmin={true} />
+
+      <div className="max-w-2xl mx-auto mt-8">
+        <Link href={"/menu-items"} className="button">
+          <Left />
+          <span>Show all menu items</span>
+        </Link>
+      </div>
+
+      <form onSubmit={handleFormSubmit} className="mt-8 max-w-2xl mx-auto ">
+        <div
+          style={{ gridTemplateColumns: ".3fr .7fr" }}
+          className="gird items-start gap-4"
+        >
+          <div>
+            <EditableImage link={image} setLink={setImage} />
+          </div>
+          <div className="grow">
+            <label>Item name</label>
+            <input
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+              type="text"
+            />
+            <label>Description</label>
+            <input
+              value={description}
+              onChange={(ev) => setDescription(ev.target.value)}
+              type="text"
+            />
+            <label>Base price</label>
+            <input
+              value={basePrice}
+              onChange={(ev) => setBasePrice(ev.target.value)}
+              type="text"
+            />
+            <button type="submit">Save</button>
           </div>
         </div>
-      </section>
-    );
+      </form>
+    </section>
+  );
 }
