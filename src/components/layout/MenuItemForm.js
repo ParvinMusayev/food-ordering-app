35348fc7aa4c74acdf1636a21f12fa188d710b5b
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-09 19:01:24 +0400

    chore: update menu item form

--- src/components/layout/MenuItemForm.js
+++ src/components/layout/MenuItemForm.js
@@ -8,6 +8,22 @@ export default function MenuItemForm({ onSubmit, menuItem }) {
   const [name, setName] = useState(menuItem?.name || "");
   const [description, setDescription] = useState(menuItem?.description || "");
   const [basePrice, setBasePrice] = useState(menuItem?.basePrice || "");
+  const [sizes, setSizes] = useState([]);
+
+  function addSizes() {
+    setSizes((oldSizes) => {
+      return [...oldSizes, { name: "", price: 0 }];
+    });
+  }
+
+  function editSize(ev, index, prop){
+    const newValue = ev.target.value
+    setSizes(prevSizes => {
+      const newSizes = [...prevSizes]
+      newSizes[index][prop] = newValue
+      return newSizes
+    })
+  }
 
   return (
     <form
@@ -17,7 +33,6 @@ export default function MenuItemForm({ onSubmit, menuItem }) {
           name,
           description,
           basePrice,
-          
         })
       }
       className="mt-8 max-w-2xl mx-auto "
@@ -48,6 +63,39 @@ export default function MenuItemForm({ onSubmit, menuItem }) {
             onChange={(ev) => setBasePrice(ev.target.value)}
             type="text"
           />
+          <div className="bg-gray-200 p-2 rounded-md mb-2">
+            <label>Sizes</label>
+            {sizes?.length > 0 &&
+              sizes.map((size, index) => (
+                <div className="flex items-end gap-2">
+                  <div>
+                    <label>Size name</label>
+                    <input
+                      type="text"
+                      placeholder="Size name"
+                      value={size.name}
+                      onChange={(ev) => editSize(ev, index, "name")}
+                    />
+                  </div>
+
+                  <div>
+                    <label>Extra price</label>
+                    <input
+                      type="text"
+                      placeholder="Extra price"
+                      value={size.price}
+                      onChange={(ev) => editSize(ev, index, "price")}
+                    />
+                  </div>
+                  <div>
+                    <button type="button" className="bg-white mb-2">x</button>
+                  </div>
+                </div>
+              ))}
+            <button type="button" onClick={addSizes} className="bg-white">
+              Add item size
+            </button>
+          </div>
           <button type="submit">Save</button>
         </div>
       </div>
