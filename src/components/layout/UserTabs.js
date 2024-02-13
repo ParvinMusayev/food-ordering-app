88ff1596dc00cb3481cea3518b80198640c43f85
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-13 15:43:06 +0400

    chore: update user tabs

--- src/components/layout/UserTabs.js
+++ src/components/layout/UserTabs.js
@@ -1,34 +1,39 @@
 "use client";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function UserTabs({ isAdmin }) {
   const path = usePathname();
   return (
-    <div className="flex mx-auto justify-center gap-2 tabs">
+    <div className="flex mx-auto gap-2 tabs justify-center flex-wrap">
       <Link className={path === "/profile" ? "active" : ""} href={"/profile"}>
         Profile
       </Link>
       {isAdmin && (
         <>
           <Link
-            className={path === "/categories" ? "active" : ""}
             href={"/categories"}
+            className={path === "/categories" ? "active" : ""}
           >
             Categories
           </Link>
           <Link
-            className={path.includes("menu-items") ? "active" : ""}
             href={"/menu-items"}
+            className={path.includes("menu-items") ? "active" : ""}
           >
             Menu Items
           </Link>
-          <Link className={path === "/users" ? "active" : ""} href={"/users"}>
+          <Link
+            className={path.includes("/users") ? "active" : ""}
+            href={"/users"}
+          >
             Users
           </Link>
         </>
       )}
+      <Link className={path === "/orders" ? "active" : ""} href={"/orders"}>
+        Orders
+      </Link>
     </div>
   );
 }
